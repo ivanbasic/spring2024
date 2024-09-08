ae28be913c3f146c518b02cac8d7a7bdbe3ae676
Ivan Basic <ivanbasic@ns>
2024-09-08 16:01:40 +0200

    007 ponovo probao nesto nece, sve radi

--- src/main/java/com/ivanbasic/learnspring/LearnSpringApplication.java
+++ src/main/java/com/ivanbasic/learnspring/LearnSpringApplication.java
@@ -1,5 +1,7 @@
 package com.ivanbasic.learnspring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +12,7 @@ import java.util.Arrays;
 
 @SpringBootApplication
 public class LearnSpringApplication {
+	private static final Logger log = LoggerFactory.getLogger(LearnSpringApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearnSpringApplication.class, args);
@@ -25,10 +28,49 @@ public class LearnSpringApplication {
 			Arrays.sort(beanNames);
 			for (String beanName : beanNames) {
 				// I have put them all in changelog
-				//System.out.println(beanName);
+				System.out.println(beanName);
 			}
 
 		};
 	}
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return (args) -> {
+			// save a few customers
+			repository.save(new Customer("Jack", "Bauer"));
+			repository.save(new Customer("Chloe", "O'Brian"));
+			repository.save(new Customer("Kim", "Bauer"));
+			repository.save(new Customer("David", "Palmer"));
+			repository.save(new Customer("Michelle", "Dessler"));
+
+			// fetch all customers
+			log.info("Customers found with findAll():");
+			log.info("-------------------------------");
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+			log.info("");
+
+			// fetch an individual customer by ID
+			Customer customer = repository.findById(1L);
+			log.info("Customer found with findById(1L):");
+			log.info("--------------------------------");
+			log.info(customer.toString());
+			log.info("");
+
+			// fetch customers by last name
+			log.info("Customer found with findByLastName('Bauer'):");
+			log.info("--------------------------------------------");
+			repository.findByLastName("Bauer").forEach(bauer -> {
+				log.info(bauer.toString());
+			});
+			// for (Customer bauer : repository.findByLastName("Bauer")) {
+			// 	log.info(bauer.toString());
+			// }
+			log.info("");
+		};
+	}
+
 
 }
