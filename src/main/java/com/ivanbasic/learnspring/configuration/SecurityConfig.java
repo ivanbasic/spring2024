9d822ab921da1c37f8cbe0925463e5e6451efcf8
Ivan Basic <ivanbasic@ns>
2024-12-17 21:13:49 +0100

    030 custom basic authorization

--- src/main/java/com/ivanbasic/learnspring/configuration/SecurityConfig.java
+++ src/main/java/com/ivanbasic/learnspring/configuration/SecurityConfig.java
@@ -1,9 +1,9 @@
 package com.ivanbasic.learnspring.configuration;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,15 +13,15 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
 
     @Bean
-    //@Order(SecurityProperties.BASIC_AUTH_ORDER)
-    @Order(SecurityProperties.IGNORED_ORDER)
     public SecurityFilterChain mySecurityFilterChain(HttpSecurity http) throws Exception {
+        log.info("inside mySecurityFilterChain");
+
         http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated());
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
-
         return http.build();
     }
 
