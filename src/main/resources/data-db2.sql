5f9b6e0cb3da082c65b06c4163773d34bf5f96ca
Ivan Basic <ivanbasic@ns>
2026-02-02 09:23:13 +0100

    054a data fix for employees

--- src/main/resources/data-db2.sql
+++ src/main/resources/data-db2.sql
@@ -9,4 +9,4 @@ INSERT INTO schema2.employees (employee_id,first_name, hire_date, department_id)
        VALUES (2, 'Niklaus', '2016-11-21 14:30:15',1);
 
 INSERT INTO schema2.employees (employee_id,first_name, hire_date, department_id)
-       VALUES (2, 'Karl', '2016-11-21 14:30:15',2);
+       VALUES (3, 'Karl', '2016-11-21 14:30:15',2);
